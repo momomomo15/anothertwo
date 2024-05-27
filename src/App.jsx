177957918ece964641779f240f38ed896db8b808
Monica Vergara <monicavergara@Monicas-MacBook-Air.local>
2024-05-27 11:29:17 -0400

    any

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import Navbar from "./components/Navbar"
+import Navbar2 from "./components/Navbar2"
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>hi world</h1>
+        <h1></h1>
+        <Navbar/>
+        <Navbar2></Navbar2>
       </div>
     </>
   )
---
+++ src/components/Navbar2.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css'; // Optional: Add a CSS file for styling
+
+const Navbar2 = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a href="/" className="navbar-item">MyWebsite</a>
+      </div>
+      <div className="navbar-menu">
+        <a href="/" className="navbar-item">Home</a>
+        <a href="/about" className="navbar-item">About</a>
+        <a href="/services" className="navbar-item">Services</a>
+        <a href="/contact" className="navbar-item">Contact</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar2;
